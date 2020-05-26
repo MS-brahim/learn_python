fde07ff762f15c971b68c48e8a37259a4d8afe0b
ms-brahim <brahim7khalil@gmail.com>
2020-05-26 18:26:28 +0000

    if..else

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
 name = "brahim"
 wd = "ASFI"
-a = 33
+a = 330
 b = 100
 
 if b > a:
     print("b is greater than a")
+elif a == b:
+    print("a and b are egual")
+else:
+    print("a is greater than b")
 print ('hello ' + wd)
 print("Mon nom est " + name)
 print("Mouissi " + name)
