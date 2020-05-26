4d340d025cf33252bea9da038ac35ee2ca3683fb
ms-brahim <brahim7khalil@gmail.com>
2020-05-26 18:31:15 +0000

    loops

--- test.py
+++ test.py
@@ -2,13 +2,23 @@ name = "brahim"
 wd = "ASFI"
 a = 330
 b = 100
+i = 1
 
+# condition
 if b > a:
     print("b is greater than a")
 elif a == b:
     print("a and b are egual")
 else:
     print("a is greater than b")
+
+# loops
+while i < 6:
+    print(i)
+    if i ==4:
+     break
+    i += 1
+# print 
 print ('hello ' + wd)
 print("Mon nom est " + name)
 print("himbra " + name)
