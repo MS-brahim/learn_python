12414e9871e478710494e57514532aa3fd2e9b18
ms-brahim <brahim7khalil@gmail.com>
2020-05-26 18:18:30 +0000

    if..else

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 name = "brahim"
 wd = "ASFI"
+a = 33
+b = 100
+
+if b > a:
+    print("b is greater than a")
 print ('hello ' + wd)
 print("Mon nom est " + name)
 print("Mouissi " + name)
-print("My Name is " + name);
+print("My Name is " + name)
